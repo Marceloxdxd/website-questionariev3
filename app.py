c9bd340d086cf49e2304bc2a78dd4eb3cc2c73f0
Marcelo Marcos <141942985+Marceloxdxd@users.noreply.github.com>
2024-06-29 07:58:09 +0000

    chore: Add lazy loading for images and update product images URLs

--- app.py
+++ app.py
@@ -7,33 +7,37 @@ ROPAS = [
         "id": 1,
         "nombre": "Pantalon",
         "precio": 100,
-        "descripcion": "Pantalon de mezclilla"
+        "descripcion": "Pantalon de mezclilla",
+        "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     },
     {
         "id": 2,
         "nombre": "Camisa",
         "precio": 200,
-        "descripcion": "Camisa de vestir"
+        "descripcion": "Camisa de vestir",
+        "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     },
     {
         "id": 3,
         "nombre": "Playera",
         "precio": 50,
-        "descripcion": "Playera de algodon"
+        "descripcion": "Playera de algodon",
+        "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     },
     {
         "id": 4,
         "nombre": "Chamarra",
         "precio": 300,
-        "descripcion": "Chamarra de piel"
+        "descripcion": "Chamarra de piel",
+        "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     },
     {
         "id": 5,
         "nombre": "Sudadera",
         "precio": 150,
-        "descripcion": "Sudadera con capucha"
+        "descripcion": "Sudadera con capucha",
+        "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     }
-
 ]
 
 @app.route("/")
