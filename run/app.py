0f4df47842bd9420174a80ce7fd1a60432963d93
Marcelo Marcos <141942985+Marceloxdxd@users.noreply.github.com>
2024-07-24 19:04:36 +0000

    chore: Refactor app.py to add database integration and seed initial data

--- run/app.py
+++ run/app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.igekterrexlcotjfvpos:dF3a1Xn8W8XvHQhg@aws-0-us-east-1.pooler.supabase.com:6543/postgres'
+
+db.init_app(app)
+
+class Ropa(db.Model):
+    __tablename__ = 'ropa'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(), nullable=False)
+    precio = db.Column(db.Integer, nullable=False)
+    descripcion = db.Column(db.String(), nullable=False)
+    imagen = db.Column(db.String(), nullable=False)
 
 ROPAS = [
     {
@@ -39,6 +57,16 @@ ROPAS = [
         "imagen": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com.mx%2FLevis-501-Original-Fit-Jeans%2Fdp%2FB0018ON4ZI&psig=AOvVaw0"
     }
 ]
+
+with app.app_context():
+    db.create_all()
+
+    for ropa in ROPAS:
+        db.session.add(Ropa(nombre=ropa['nombre'], precio=ropa['precio'], descripcion=ropa['descripcion'], imagen=ropa['imagen']))
+
+    db.session.commit()
+
+
 
 @app.route("/")
 def hello():
